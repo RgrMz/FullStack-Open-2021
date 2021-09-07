5912e57ca214b92419925830220c37d0d41e86ed
RgrMz <cargmzrub@gmail.com>
2021-09-07 16:24:23 +0200

    Add exercise 2.3

--- part2/course-information/src/components/Total.js
+++ part2/course-information/src/components/Total.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+    const sum = 
+      course.parts.reduce((total, part) => total + part.exercises, 0)
     return(
       <p>Number of exercises {sum}</p>
     ) 
